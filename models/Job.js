de85e55f8107269d277ef997f1986dab2b1d5463
Osman <114867365+osmansam@users.noreply.github.com>
2023-08-05 17:53:58 -0400

    authenticaion created for register,login and logout

---
+++ models/Job.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending", "hired"],
+      default: "pending",
+    },
+    jobType: {
+      type: String,
+      enum: ["full-time", "part-time", "remote", "internship"],
+      default: "full-time",
+    },
+    jobLocation: {
+      type: String,
+      default: "remote",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", JobSchema);
