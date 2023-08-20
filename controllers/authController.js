d91fff8a1d8d657d18fe3bb3dd4c5b6d53fe8475
Osman <114867365+osmansam@users.noreply.github.com>
2023-08-20 13:34:36 -0500

    google login solved

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const Token = require("../models/Token");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const jwtDecode = require("jwt-decode");
 const { attachCookiesToResponse, createTokenUser } = require("../utils");
 
 const crypto = require("crypto");
@@ -80,6 +81,57 @@ const login = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
+//Google Login
+const googleLogin = async (req, res) => {
+  const { tokenId } = req.body;
+
+  const { email_verified, family_name, given_name, email, picture } =
+    await jwtDecode(tokenId);
+  if (!email_verified) {
+    throw new CustomError.UnauthenticatedError("Google login failed");
+  }
+  console.log(email_verified, family_name, given_name, email, picture);
+  let user = await User.findOne({ email });
+
+  if (user.length === 0) {
+    // If the user doesn't exist, create a new user account
+    user = await User.create({
+      name: given_name,
+      lastName: family_name,
+      email: email,
+      picture: picture,
+      role: "user",
+      password: email + process.env.JWT_SECRET,
+    });
+  }
+  const tokenUser = createTokenUser(user);
+  // create refresh token
+  let refreshToken = "";
+  // check for existing token
+  const existingToken = await Token.findOne({ user: user._id });
+
+  if (existingToken) {
+    const { isValid } = existingToken;
+    if (!isValid) {
+      throw new CustomError.UnauthenticatedError("Invalid Credentials");
+    }
+    refreshToken = existingToken.refreshToken;
+    attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+    res.status(StatusCodes.OK).json({ user: tokenUser });
+    return;
+  }
+
+  refreshToken = crypto.randomBytes(40).toString("hex");
+  const userAgent = req.headers["user-agent"];
+  const ip = req.ip;
+  const userToken = { refreshToken, ip, userAgent, user: user._id };
+
+  await Token.create(userToken);
+
+  attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+
+  res.status(StatusCodes.OK).json({ user: tokenUser });
+};
 
 //LOGOUT
 const logout = async (req, res) => {
@@ -100,5 +152,6 @@ const logout = async (req, res) => {
 module.exports = {
   register,
   login,
+  googleLogin,
   logout,
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { authenticateUser } = require("../middleware/authentication");
 
-const { register, login, logout } = require("../controllers/authController");
+const {
+  register,
+  login,
+  googleLogin,
+  logout,
+} = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/googleLogin", googleLogin);
 router.delete("/logout", authenticateUser, logout);
 
 module.exports = router;
